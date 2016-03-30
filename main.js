7614c9b8af5237bb5a63a0cb1ff5c7cdf4976732
buckwheat <comp322012016@mail.ru>
2016-03-30 16:46:42 +0300

    laba

---
+++ main.js
@@ -0,0 +1,12 @@
+(function() {
+var div = document.getElementsByClassName('block');
+ style = div[0].style;
+
+ style.color = "red";
+ style.backgroundColor = "black";
+ style.border="1px solid blue";
+ style.padding="3px";
+
+
+
+})();
